d3783684262c30d4cee96c7695d77c2751bccc8c
David Tumpowsky <tumpowskydavid@gmail.com>
2018-06-15 16:42:06 -0700

    minor adjustments

--- src/app/zestimate/zestimate.component.ts
+++ src/app/zestimate/zestimate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-zestimate',
@@ -6,7 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./zestimate.component.css']
 })
 export class ZestimateComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
